37f0f797a6e73ccb2935c4d25b25baab17e2daa9
Rachit Kaushik <56151848+rk-2401@users.noreply.github.com>
2021-07-29 19:50:30 +0530

    Resolve delayed audio play

--- app.js
+++ app.js
@@ -1,118 +1,5 @@
 var bgColor="#0FC0FC";
 var drumColor="#7B1DAF";
-const bankOne = {
-    81:{
-      keyCode: 81,
-      keyTrigger: 'Q',
-      id: 'heater1',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
-    },
-    87:{
-      keyCode: 87,
-      keyTrigger: 'W',
-      id: 'heater2',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
-    },
-    69:{
-      keyCode: 69,
-      keyTrigger: 'E',
-      id: 'heater3',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
-    },
-    65:{
-      keyCode: 65,
-      keyTrigger: 'A',
-      id: 'heater4',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
-    },
-    83:{
-      keyCode: 83,
-      keyTrigger: 'S',
-      id: 'clap',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
-    },
-    68:{
-      keyCode: 68,
-      keyTrigger: 'D',
-      id: 'open-hh',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
-    },
-    90:{
-      keyCode: 90,
-      keyTrigger: 'Z',
-      id: "kick-n-hat",
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
-    },
-    88:{
-      keyCode: 88,
-      keyTrigger: 'X',
-      id: 'kick',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
-    },
-    67:{
-      keyCode: 67,
-      keyTrigger: 'C',
-      id: 'closed-hh',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
-    }
-};
-  
-  const bankTwo = [
-    {
-      keyCode: 81,
-      keyTrigger: 'Q',
-      id: 'Chord-1',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
-    },
-    {
-      keyCode: 87,
-      keyTrigger: 'W',
-      id: 'Chord-2',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
-    },
-    {
-      keyCode: 69,
-      keyTrigger: 'E',
-      id: 'Chord-3',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
-    },
-    {
-      keyCode: 65,
-      keyTrigger: 'A',
-      id: 'Shaker',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
-    },
-    {
-      keyCode: 83,
-      keyTrigger: 'S',
-      id: 'Open-HH',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
-    },
-    {
-      keyCode: 68,
-      keyTrigger: 'D',
-      id: 'Closed-HH',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
-    },
-    {
-      keyCode: 90,
-      keyTrigger: 'Z',
-      id: 'Punchy-Kick',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
-    },
-    {
-      keyCode: 88,
-      keyTrigger: 'X',
-      id: 'Side-Stick',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
-    },  
-    {
-      keyCode: 67,
-      keyTrigger: 'C',
-      id: 'Snare',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
-    }
-  ];
 
   class Bank1 extends React.Component{
     constructor(props){
@@ -129,6 +16,7 @@ const bankOne = {
         bgColor=drumColor;
         drumColor=temp;
         
+        this.props.changeCurAudio(instrument[0]);
         instrument[0].play();
         $(document).ready(function(){
           $("#display").html(instrument[0].parentNode.id);
@@ -182,7 +70,7 @@ class Bank2 extends React.Component{
         var temp=bgColor;
         bgColor=drumColor;
         drumColor=temp;
-        
+        this.props.changeCurAudio(instrument[0]);
         instrument[0].play();
         $(document).ready(function(){
           $("#display").html(instrument[0].parentNode.id);
@@ -308,13 +196,22 @@ class DrumMachine extends React.Component{
         super(props);
         this.state={
             bank:1,
-            power:true
+            power:true,
+            curAudio:''
         }
 
         this.changePState=this.changePState.bind(this);
         this.changeBState=this.changeBState.bind(this);
 
         this.handleKeyPress=this.handleKeyPress.bind(this);
+        this.changeCurAudio=this.changeCurAudio.bind(this);
+    }
+    changeCurAudio(audioElem){
+      if(this.state.curAudio!=''){
+      this.state.curAudio.pause();
+      this.state.curAudio.currentTime=0;
+      }
+      this.setState({curAudio:audioElem})
     }
     changePState(){
       if(this.state.power)
@@ -331,14 +228,19 @@ class DrumMachine extends React.Component{
     handleKeyPress(e){
         if(this.state.power)  
         {
+          
           var key=e.keyCode;
           var keyTrigger=String.fromCharCode(key);
           var audioElem=document.getElementById(keyTrigger);
+          
+          this.changeCurAudio(audioElem);
+          audioElem.play();
+          
+
           var temp=bgColor;
           bgColor=drumColor;
           drumColor=temp;
 
-          audioElem.play();
           $(document).ready(function(){
             $("#display").html(audioElem.parentNode.id);
             $("html").css('background-color',bgColor);
@@ -366,8 +268,8 @@ class DrumMachine extends React.Component{
         return(
             <div id="drum-machine">
                 <div id="name">Drum Machine</div>
-                <Bank1 changePState={this.changePState} changeBState={this.changeBState} state={this.state}/>
-                <Switches changePState={this.changePState} changeBState={this.changeBState} state={this.state}/>
+                <Bank1 changeCurAudio={this.changeCurAudio} changePState={this.changePState} changeBState={this.changeBState} state={this.state}/>
+                <Switches changeCurAudio={this.changeCurAudio} changePState={this.changePState} changeBState={this.changeBState} state={this.state}/>
             </div>
         );
         else{
@@ -375,8 +277,8 @@ class DrumMachine extends React.Component{
         return(
             <div id="drum-machine">
                 <div id="name">Drum Machine</div>
-                <Bank2 changePState={this.changePState} changeBState={this.changeBState} state={this.state}/>
-                <Switches changePState={this.changePState} changeBState={this.changeBState} state={this.state}/>
+                <Bank2 changeCurAudio={this.changeCurAudio} changePState={this.changePState} changeBState={this.changeBState} state={this.state}/>
+                <Switches changeCurAudio={this.changeCurAudio} changePState={this.changePState} changeBState={this.changeBState} state={this.state}/>
             </div>
         );
         }
